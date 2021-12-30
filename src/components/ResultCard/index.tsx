85a8ee5bbe6821bc3bf92c6797a3694a8adfc7b6
Arthur Scarpati <arthur_159@hotmail.com>
2021-12-30 16:07:40 -0300

    Fixing borders

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -11,14 +11,23 @@ type Props = {
 const ResultCard = ({ foto, perfil, seguidores, localidade, nome }: Props) => {
   return (
     <div className="result-container">
-      <div className="img-container"><img src={foto} alt="foto" /></div>
+      <div className="img-container">
+        <img src={foto} alt="foto" />
+      </div>
       <div className="info-container">
-          <h1>Informações</h1>
-          <div className="card-perfil"><h4>Perfil: {perfil}</h4></div>
-          
+        <h1>Informações</h1>
+        <div className="card-perfil">
+          <h4>Perfil: {perfil}</h4>
+        </div>
+        <div className="card-perfil">
           <h4>Seguidores: {seguidores}</h4>
+        </div>
+        <div className="card-perfil">
           <h4>Localidade: {localidade}</h4>
+        </div>
+        <div className="card-perfil">
           <h4>Nome: {nome}</h4>
+        </div>
       </div>
     </div>
   );
